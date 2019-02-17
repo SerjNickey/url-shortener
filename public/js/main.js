5daba3124be67703fa2fadbfc346d74a5157622d
SerjNickey <serjnickey@gmail.com>
2019-02-17 20:59:51 +0300

    Commit 1

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.url-form');
+const result = document.querySelector('.result-section');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const input = document.querySelector('.url-input');
+  fetch('/new', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      url: input.value
+    })
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw Error(res.statusText);
+    }
+    return res.json();
+  })
+  .then(data => {
+    while (result.hasChildNodes()) {
+      result.removeChild(result.lastChild);
+    }
+
+    result.insertAdjacentHTML('afterbegin', `
+      <div class="result">
+        <a target="_blank" class="short-url" rel="noopener" href="/${data.short_id}">
+          ${location.origin}/${data.short_id}
+        </a>
+      </div>
+    `);
+  })
+  .catch(console.error)
+});
